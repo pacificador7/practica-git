b218b2482c28cab8a914dee90b8b1906d41dd913
Ghilmer Ventura <dv24003@ues.edu.sv>
2026-03-01 17:50:07 -0600

    feat: cambiar mensaje para nombre vacio en rama-b

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function saludar(nombre) {
-  if (!nombre) return 'Hola, visitante';
+  if (!nombre) return 'Hola, visitante desconocido';
   return 'Hola, ' + nombre;
 }
 function despedir(nombre) { return 'Adiós, ' + nombre; }
